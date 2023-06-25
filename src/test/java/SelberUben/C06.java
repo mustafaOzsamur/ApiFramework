de99837e816be2591882722feeb5428083fdbb40
MUSTAFA ÖZSAMUR <mustafaozsamur22@gmail.com>
2023-06-26 00:35:18 +0200

    26.06V1

---
+++ src/test/java/SelberUben/C06.java
@@ -0,0 +1,114 @@
+package SelberUben;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C06 {
+
+
+
+    //api/opdList endpoint'ine gecerli authorization bilgileri ile bir GET request gönderildiginde
+// dönen status code'un 200 oldugu ve response message bilgisinin "Success" oldugu dogrulanmali
+
+    @Test
+    public void test01() {
+
+            // ilk is spec ile base url olusturulur
+            RequestSpecification spec = new RequestSpecBuilder().setBaseUri("https://heallifehospital.com/").build();
+            String token = "";
+            spec.pathParams("pp1","api","pp2","opdList");
+            String fullPath ="/{pp1}/{pp2}";
+            Response response = given()
+                    .contentType(ContentType.JSON)
+                    .spec(spec)
+                    .headers(
+                            "Authorization",
+                            "Bearer "+ token,
+                            "Content-Type",
+                            ContentType.JSON,
+                            "Accept",
+                            ContentType.JSON
+                    )
+                    .when()
+                    .get(fullPath);
+            response.prettyPrint();
+
+            response.then().assertThat().statusCode(200).body("message", Matchers.equalTo("Success"));
+        }
+
+
+        @Test
+    public void test02(){
+
+            //api/opdList endpoint'ine gecersiz authorization bilgileri ile bir GET Request gönderildiginde
+            // dönen status code'un 403 oldugu ve response message bilgisinin "failed" oldugu dogrulanmali
+            RequestSpecification spec = new RequestSpecBuilder().setBaseUri("https://heallifehospital.com/").build();
+            String token= "lkşjdfsaşlkjdfşaıejrıoqwupew";
+            spec.pathParams("pp1","api","pp2","opdList");
+            String fullPath ="/{pp1}/{pp2}";
+            // hata olarak 403 bize döndüğü için system exception fırlatıyor. Exceptionın mesajını kaydedip onun üzerinden
+            //test yapacağız
+            String exceptionMsj ="";
+            try {
+                Response response = given()
+                        .contentType(ContentType.JSON)
+                        .spec(spec)
+                        .headers(
+                                "Authorization",
+                                "Bearer " + token,
+                                "Content-Type",
+                                ContentType.JSON,
+                                "Accept",
+                                ContentType.JSON
+                        )
+                        .when()
+                        .get(fullPath);
+            } catch (Exception e) {
+                exceptionMsj = e.getMessage();
+            }
+            System.out.println(exceptionMsj);
+            Assert.assertTrue(exceptionMsj.contains("status code: 403"));
+        }
+
+        @Test
+        public void tokenCreate(){
+
+            String url="https://www.heallifehospital.com/api/getToken/";
+
+            JSONObject reqBody=new JSONObject();
+
+            reqBody.put("email","hatice.kubra.ceylan.admin@heallifehospital.com");
+            reqBody.put("password", "heallife123");
+
+
+
+
+            Response response= given()
+                    .contentType(ContentType.JSON)
+                    .when()
+                    .body(reqBody.toString())
+                    .post(url);
+
+            response.prettyPrint();
+
+            //response icinde token i bulabilmemiz icin öncelikle responsu JsonPath objesine cevirmeliyiz
+
+            JsonPath jsonResponse=response.jsonPath();
+
+            String token=jsonResponse.getString("token");
+
+            System.out.println(token);
+
+
+        }
+    }
---
+++ src/test/java/test/C24_Post_DeserializationPOJO.java
@@ -0,0 +1,8 @@
+package test;
+
+public class C24_Post_DeserializationPOJO {
+    /*
+    https://restful-booker.herokuapp.com/booking url’ine asagidaki body'ye sahip bir POST
+    request gonderdigimizde donen response’un id haric asagidaki gibi oldugunu test edin.
+     */
+}
